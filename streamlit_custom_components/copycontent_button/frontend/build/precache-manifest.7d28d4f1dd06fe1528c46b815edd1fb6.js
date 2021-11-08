104594336542e3028a4a1a186866e3509ba08996
JJGarcia24 <40121872+jasonjgarcia24@users.noreply.github.com>
2021-11-08 00:11:53 -0800

    Removed 'build/' from .gitignore for custom streamlit components.

---
+++ streamlit_custom_components/copycontent_button/frontend/build/precache-manifest.7d28d4f1dd06fe1528c46b815edd1fb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec1c9b176a2b7dd6d7b7e2ae3bf9f2e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c00930febb7fe2c9e125",
+    "url": "./static/js/2.bd9b94bf.chunk.js"
+  },
+  {
+    "revision": "bdefffd1d670660407b69ff965a47428",
+    "url": "./static/js/2.bd9b94bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e1331bc58ef1ab37048",
+    "url": "./static/js/main.5ac0fb29.chunk.js"
+  },
+  {
+    "revision": "c39a3bbff586b7a327bc",
+    "url": "./static/js/runtime-main.547c5a5e.js"
+  }
+]);
---
+++ streamlit_custom_components/crypto_account_stack/frontend/build/precache-manifest.e4affc5c9aa6a411f03be436b6d71409.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b97845ee00117c8c702545cdd34f63a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a642c2810c6854a237f5",
+    "url": "./static/js/2.bd102428.chunk.js"
+  },
+  {
+    "revision": "bdefffd1d670660407b69ff965a47428",
+    "url": "./static/js/2.bd102428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda5a5c11bb19ecf39b9",
+    "url": "./static/js/main.453cc1a2.chunk.js"
+  },
+  {
+    "revision": "23bf9917ea6a766da04c",
+    "url": "./static/js/runtime-main.eab36c2e.js"
+  }
+]);
